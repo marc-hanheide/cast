5e0e82b2c589e2b0d2bf82aa54bb80eb80744da1
nah <nah@a400dbbb-9bcb-4e31-bf58-790d63d6b2cf>
2011-10-03 19:00:11 +0000

    update tag

--- tags/cast-2.1.16b/cmake/UseBoost.cmake
+++
@@ -1,2 +0,0 @@
-include_directories(${Boost_INCLUDE_DIR})
-link_directories(${Boost_LIBRARY_DIRS})
--- tags/cast-2.1.16b/src/python/cast/WordServerClient.py
+++
@@ -1,9 +0,0 @@
-import cast.core
-import time
-
-class WordServerClient(cast.core.CASTComponent):
-  def runComponent(self):
-    agg = self.getIceServer("aggregate.server", cast.examples.autogen.WordServer, cast.examples.autogen.WordServerPrx)
-    print "new word: ", agg.getNewWord()
-    impl = self.getIceServer("implements.server", cast.examples.autogen.WordServer, cast.examples.autogen.WordServerPrx)
-    print "new word: ", impl.getNewWord()
